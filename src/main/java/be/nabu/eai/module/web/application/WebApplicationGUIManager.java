dd62c52c95feb848b13733d598f79bc6c4d5a656
alex <alex@nabu.be>
2016-04-27 08:57:49 +0200

    Append own path

--- src/main/java/be/nabu/eai/module/web/application/WebApplicationGUIManager.java
+++ src/main/java/be/nabu/eai/module/web/application/WebApplicationGUIManager.java
@@ -15,6 +15,7 @@ import javafx.geometry.Orientation;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Separator;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import be.nabu.eai.developer.MainController;
@@ -67,93 +68,111 @@ public class WebApplicationGUIManager extends BaseJAXBGUIManager<WebApplicationC
 			Set<String> allTypes = new HashSet<String>();
 			// saves a lookup later on
 			Map<String, ComplexType> typeDefinitions = new HashMap<String, ComplexType>();
-			for (WebFragment fragment : artifact.getConfiguration().getWebFragments()) {
-				for (final WebFragmentConfiguration fragmentConfiguration : fragment.getFragmentConfiguration()) {
-					String typeId = ((DefinedType) fragmentConfiguration.getType()).getId();
-					typeDefinitions.put(typeId, fragmentConfiguration.getType());
-					allPaths.add(fragmentConfiguration.getPath());
-					allTypes.add(typeId);
-					boolean found = false;
-					WebConfigurationPart typeMatch = null;
-					for (WebConfigurationPart configuration : webConfiguration.getParts()) {
-						if (configuration.getType().equals(typeId)) {
-							typeMatch = configuration;
-							if (configuration.getPaths().contains(fragmentConfiguration.getPath())) {
-								found = true;
-								break;
+			if (artifact.getConfiguration().getWebFragments() != null) {
+				for (WebFragment fragment : artifact.getConfiguration().getWebFragments()) {
+					for (final WebFragmentConfiguration fragmentConfiguration : fragment.getFragmentConfiguration()) {
+						String typeId = ((DefinedType) fragmentConfiguration.getType()).getId();
+						typeDefinitions.put(typeId, fragmentConfiguration.getType());
+						String path = fragmentConfiguration.getPath();
+						if (artifact.getConfiguration().getPath() != null && !artifact.getConfiguration().getPath().isEmpty() && !"/".equals(artifact.getConfiguration().getPath())) {
+							path = artifact.getConfiguration().getPath().replaceFirst("[/]+$", "") + "/" + path.replaceFirst("^[/]+", "");
+							if (!path.startsWith("/")) {
+								path = "/" + path;
 							}
 						}
-					}
-					// did not find configuration for this fragment
-					if (!found) {
-						// found a similar type configuration, use that
-						if (typeMatch != null) {
-							typeMatch.getPaths().add(fragmentConfiguration.getPath());
-						}
-						// add a new one
-						else {
-							WebConfigurationPart newPart = new WebConfigurationPart();
-							newPart.setType(typeId);
-							newPart.getPaths().add(fragmentConfiguration.getPath());
-							webConfiguration.getParts().add(newPart);
-						}
-					}
-				}
-			}
-			// remove unused paths and/or types
-			Iterator<WebConfigurationPart> iterator = webConfiguration.getParts().iterator();
-			while (iterator.hasNext()) {
-				WebConfigurationPart next = iterator.next();
-				if (!allTypes.contains(next.getType())) {
-					iterator.remove();
-				}
-				else {
-					next.getPaths().retainAll(allPaths);
-				}
-			}
-			
-			for (final WebConfigurationPart configuration : webConfiguration.getParts()) {
-				VBox box = new VBox();
-				box.getChildren().addAll(new Separator(Orientation.HORIZONTAL), new Label(configuration.getType()));
-				List<Property<?>> properties = BaseConfigurationGUIManager.createProperty(new ComplexElementImpl(typeDefinitions.get(configuration.getType()), null));
-				List<Value<?>> values = new ArrayList<Value<?>>();
-				for (Property<?> property : properties) {
-					if (configuration.getConfiguration().containsKey(property.getName())) {
-						values.add(new ValueImpl(property, ConverterFactory.getInstance().getConverter().convert(configuration.getConfiguration().get(property.getName()), property.getValueClass())));
-					}
-				}
-				AnchorPane childPane = new AnchorPane();
-				SimplePropertyUpdater updater = new SimplePropertyUpdater(true, new LinkedHashSet<Property<?>>(properties), values.toArray(new Value[values.size()]));
-				updater.valuesProperty().addListener(new ListChangeListener<Value<?>>() {
-					@Override
-					public void onChanged(ListChangeListener.Change<? extends Value<?>> change) {
-						while (change.next()) {
-							if (change.wasRemoved()) {
-								for (Value<?> value : change.getRemoved()) {
-									configuration.getConfiguration().remove(value.getProperty().getName());
-								}
-							}
-							if (change.wasAdded()) {
-								for (Value value : change.getAddedSubList()) {
-									configuration.getConfiguration().put(value.getProperty().getName(), ConverterFactory.getInstance().getConverter().convert(value.getValue(), String.class));
-								}
-							}
-							if (change.wasUpdated() || change.wasReplaced()) {
-								for (Value value : change.getList()) {
-									configuration.getConfiguration().put(value.getProperty().getName(), ConverterFactory.getInstance().getConverter().convert(value.getValue(), String.class));
+						allPaths.add(path);
+						allTypes.add(typeId);
+						boolean found = false;
+						WebConfigurationPart typeMatch = null;
+						for (WebConfigurationPart configuration : webConfiguration.getParts()) {
+							if (configuration.getType().equals(typeId)) {
+								typeMatch = configuration;
+								if (configuration.getPaths().contains(path)) {
+									found = true;
+									break;
 								}
 							}
 						}
+						// did not find configuration for this fragment
+						if (!found) {
+							// found a similar type configuration, use that
+							if (typeMatch != null) {
+								typeMatch.getPaths().add(path);
+							}
+							// add a new one
+							else {
+								WebConfigurationPart newPart = new WebConfigurationPart();
+								newPart.setType(typeId);
+								newPart.getPaths().add(path);
+								webConfiguration.getParts().add(newPart);
+							}
+						}
 					}
-				});
-				MainController.getInstance().showProperties(
-					updater, 
-					childPane, 
-					true, 
-					artifact.getRepository()
-				);
-				box.getChildren().add(childPane);
-				vbox.getChildren().add(box);
+				}
+				// remove unused paths and/or types
+				Iterator<WebConfigurationPart> iterator = webConfiguration.getParts().iterator();
+				while (iterator.hasNext()) {
+					WebConfigurationPart next = iterator.next();
+					if (!allTypes.contains(next.getType())) {
+						iterator.remove();
+					}
+					else {
+						next.getPaths().retainAll(allPaths);
+					}
+				}
+				
+				for (final WebConfigurationPart configuration : webConfiguration.getParts()) {
+					VBox box = new VBox();
+					Label label = new Label(configuration.getType());
+					StringBuilder pathBuilder = new StringBuilder();
+					for (String path : configuration.getPaths()) {
+						if (!pathBuilder.toString().isEmpty()) {
+							pathBuilder.append(", ");
+						}
+						pathBuilder.append(path);
+					}
+					label.setTooltip(new Tooltip(pathBuilder.toString()));
+					box.getChildren().addAll(new Separator(Orientation.HORIZONTAL), label);
+					List<Property<?>> properties = BaseConfigurationGUIManager.createProperty(new ComplexElementImpl(typeDefinitions.get(configuration.getType()), null));
+					List<Value<?>> values = new ArrayList<Value<?>>();
+					for (Property<?> property : properties) {
+						if (configuration.getConfiguration().containsKey(property.getName())) {
+							values.add(new ValueImpl(property, ConverterFactory.getInstance().getConverter().convert(configuration.getConfiguration().get(property.getName()), property.getValueClass())));
+						}
+					}
+					AnchorPane childPane = new AnchorPane();
+					SimplePropertyUpdater updater = new SimplePropertyUpdater(true, new LinkedHashSet<Property<?>>(properties), values.toArray(new Value[values.size()]));
+					updater.valuesProperty().addListener(new ListChangeListener<Value<?>>() {
+						@Override
+						public void onChanged(ListChangeListener.Change<? extends Value<?>> change) {
+							while (change.next()) {
+								if (change.wasRemoved()) {
+									for (Value<?> value : change.getRemoved()) {
+										configuration.getConfiguration().remove(value.getProperty().getName());
+									}
+								}
+								if (change.wasAdded()) {
+									for (Value value : change.getAddedSubList()) {
+										configuration.getConfiguration().put(value.getProperty().getName(), ConverterFactory.getInstance().getConverter().convert(value.getValue(), String.class));
+									}
+								}
+								if (change.wasUpdated() || change.wasReplaced()) {
+									for (Value value : change.getList()) {
+										configuration.getConfiguration().put(value.getProperty().getName(), ConverterFactory.getInstance().getConverter().convert(value.getValue(), String.class));
+									}
+								}
+							}
+						}
+					});
+					MainController.getInstance().showProperties(
+						updater, 
+						childPane, 
+						true, 
+						artifact.getRepository()
+					);
+					box.getChildren().add(childPane);
+					vbox.getChildren().add(box);
+				}
 			}
 		}
 		catch (IOException e) {
